191610ff7d437547fb5e098796e2701a74889b2e
Fyodor Danilov <danilovfa@protonmail.com>
2023-08-07 13:07:17 +0300

    Initial commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+    id("app.cash.sqldelight")
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+
+                implementation("app.cash.sqldelight:runtime:2.0.0")
+                implementation("app.cash.sqldelight:coroutines-extensions:2.0.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("io.insert-koin:koin-core:3.4.2")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.core:core:1.10.1")
+                implementation("app.cash.sqldelight:android-driver:2.0.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("TasksDatabase") {
+            packageName.set("com.danilovfa.organizer.tasksDatabase")
+        }
+    }
+}
+
+android {
+    namespace = "com.danilovfa.organizer"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
