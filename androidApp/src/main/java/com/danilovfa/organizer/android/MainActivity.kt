fe17297b333cd2d1d3cb3f1122ac8782fa8cf3e9
Fyodor Danilov <danilovfa@protonmail.com>
2023-08-11 14:56:02 +0300

    Add root component

--- androidApp/src/main/java/com/danilovfa/organizer/android/MainActivity.kt
+++ androidApp/src/main/java/com/danilovfa/organizer/android/MainActivity.kt
@@ -3,12 +3,27 @@ package com.danilovfa.organizer.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import com.arkivanov.decompose.defaultComponentContext
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import com.danilovfa.organizer.core.presentation.OrganizerTheme
+import com.danilovfa.organizer.ui.root.RootComponent
+import com.danilovfa.organizer.ui.root.RootContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        val rootComponent = RootComponent(
+            componentContext = defaultComponentContext(),
+            storeFactory = DefaultStoreFactory()
+        )
 
+        setContent {
+            OrganizerTheme(
+                darkTheme = isSystemInDarkTheme()
+            ) {
+                RootContent(rootComponent)
+            }
         }
     }
 }
